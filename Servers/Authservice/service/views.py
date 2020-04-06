e4598b24d5dfe44efa38f755421ddb4ef8929eef
Kim Sandberg <s@ndberg.dk>
2020-04-06 20:24:52 +0200

    Auth Service is finished for testing up against Game Service

--- Servers/Authservice/service/views.py
+++ Servers/Authservice/service/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import suds
+import requests
 
 AUTH_SERVICE_ACCESS_KEY = "HBdjm4VDLxn8mU2Eh7EzwNdhAEYp7bm9HvgwEJVGeM6NaBFvFFS48qbSHUYKLkuZPRWKxvGJsu4RewuuR6SVEEbH5aUqjD7H8wMeEPBd5d4G8UfB7QxhuTPPF8KKZg53zvUdv63ravcBAzdgPRbxcVu7pb6NPRfVLf3fFznvCX5ey2by6kGe3HrZX6kBTsJxTS6cL4KwkQDaN5YTq5jzQrQ4wLaXBYzx9y4w5sXdfkhLWuCL5wdFMtgbd8cNTemR"
 
@@ -34,13 +35,15 @@ def login(request):
 
     try:
         user = client.service.hentBruger(username, password)
-        print ("user: " + str(user))
+
         user_token = uuid.uuid1()
-        gameservice_ip = registerUserWithGameService(AUTH_SERVICE_ACCESS_KEY, user_token) == true:
+        gameservice_ip, gameservice_port = registerUserWithGameService(AUTH_SERVICE_ACCESS_KEY, user_token)
+
         if gameservice_ip != None:
             user['usertoken'] = user_token
             user['gameservice_ip'] = gameservice_ip
             user['gameservice_port'] = gameservice_port
+            print("user: " + str(user))
             return Response(user, status=status.HTTP_200_OK)
 
     except suds.WebFault as detail:
@@ -51,6 +54,15 @@ def login(request):
         #Because Python cant be bothered to implement switch statements
 
 def registerUserWithGameService(service_key, user_token):
-    gameservice_ip = "192-168.1.1"
-
-    return gameservice_ip
+    gameservice_ip = "192.168.1.1"
+    gameservice_port ="8100"
+    URL = "http://127.0.0.1:8010/registeruser"
+    PARAMS = {"servicekey":service_key, "usertoken":user_token}
+    try:
+        r = requests.post(url = URL, params = PARAMS)
+        data = r.json()
+        gameservice_ip = data['gameservice_ip']
+        gameservice_port = data['gameservice_port']
+    except requests.ConnectionError as e:
+        return None, None
+    return gameservice_ip, gameservice_port
