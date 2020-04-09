760e71815f52709689eb1c6bfb19f78d4bf84c05
Kim Sandberg <s@ndberg.dk>
2020-04-09 08:04:17 +0200

    Now gives the proper APX feedback

--- Servers/Authservice/service/views.py
+++ Servers/Authservice/service/views.py
@@ -25,44 +25,62 @@ def index(request):
 def login(request):
 
     print("login post")
+    #get data from the json posted to the method
     decoded = request.body.decode('utf-8')
     response = json.loads(decoded)
-    username = response['username']
-    password = response['password']
+
+    try:
+        username = response['username']
+        password = response['password']
+    except KeyError:
+        print("400 badrequest")
+        return Response(data={"reason": "Your data is not formatted the proper way",
+                              "helper": "[POST] at /login and then a JSON formatted like this, {'username':'yourUserName','password':'yourPassword'}"}, status=status.HTTP_400_BAD_REQUEST)
+        # Here can we have more if statements for further error handling.
+        # Because Python cant be bothered to implement switch statements
+
+    #Create a soap client object
     url = 'http://javabog.dk:9901/brugeradmin?wsdl'
     client = Client(url, faults=True)
     user_token = None
 
+    #try and get the userobject from javabog.dk
     try:
+        print("OK")
+        #Get user and create token
         user = client.service.hentBruger(username, password)
-
         user_token = uuid.uuid1()
+
+        #Register user at gameservice and get gameservice ip and port
         gameservice_ip, gameservice_port = registerUserWithGameService(AUTH_SERVICE_ACCESS_KEY, user_token)
 
+        #If gameservice responds with proper data, return the userobject, usertoken and gameservice ip and port to client.
         if gameservice_ip != None:
             user['usertoken'] = user_token
             user['gameservice_ip'] = gameservice_ip
             user['gameservice_port'] = gameservice_port
             print("user: " + str(user))
-            return Response(user, status=status.HTTP_200_OK)
 
+            return Response(user, status=status.HTTP_200_OK)
+    #if the communication with javabog.dk did not go through as expected
     except suds.WebFault as detail:
         if "Forkert brugernavn eller adgangskode" in str(detail):
+            print("401 bad credentials")
             print("detail: " + str(detail))
-            return Response("NULL", status=status.HTTP_400_BAD_REQUEST)
-        #Here can we have more if statements for further error handling.
-        #Because Python cant be bothered to implement switch statements
+            return Response(data={"reason":"Your credentials were not correct"}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 def registerUserWithGameService(service_key, user_token):
-    gameservice_ip = "192.168.1.1"
-    gameservice_port ="8100"
+
     URL = "http://127.0.0.1:8010/registeruser"
     PARAMS = {"servicekey":service_key, "usertoken":user_token}
     try:
-        r = requests.post(url = URL, params = PARAMS)
-        data = r.json()
-        gameservice_ip = data['gameservice_ip']
-        gameservice_port = data['gameservice_port']
+        #r = requests.post(url = URL, params = PARAMS)
+        #data = r.json()
+        #gameservice_ip = data['gameservice_ip']
+        #gameservice_port = data['gameservice_port']
+        gameservice_ip = "192.168.1.1"
+        gameservice_port = "8100"
     except requests.ConnectionError as e:
         return None, None
     return gameservice_ip, gameservice_port
