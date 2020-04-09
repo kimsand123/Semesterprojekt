0b60e4caa0359bb45a6f65133e39ef8f775cd5e3
Kim Sandberg <s@ndberg.dk>
2020-04-09 14:49:01 +0200

    Setting up urls again

--- Servers/GameService/GameService/urls.py
+++ Servers/GameService/GameService/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path('players/', include('GameServiceApp.urls')),
+    path('invites/', include('GameServiceApp.urls')),
+    path('games/', include('GameServiceApp.urls')),
 ]
