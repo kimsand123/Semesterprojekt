88daed34f806a24589778a07441e194b0d7d8537
Sebastian Stokkebro <stokkebro.s@gmail.com>
2020-04-14 14:35:10 +0200

    Added game endpoints

--- Servers/GameService/GameServiceApp/urls.py
+++ Servers/GameService/GameServiceApp/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
         path('invites/<str:token>', views.get_invites), #GET all invites
         path('invites/<str:token>', views.invite_player), #POST invite a specific player
         path('invites/<str:token>', views.accept_invite), #PUT accept an invite
+
+        path('games/', views.get_games), #GET get all games
+        path('games/<int:game_id>/', views.get_game), #GET get single game
+        path('games/<int:game_id>/player_status', views.get_all_player_status), #GET get all player status
+        path('games/<int:game_id>/player_status/<int:player_id>/status', views.set_player_status), #POST player status
     ]
