2e5adc510194b1fe6567f11182683744e02af9f2
Sebastian Stokkebro <stokkebro.s@gmail.com>
2020-05-10 12:25:10 +0200

    Added cors whitelist for gq.stokkebro.dev

--- Servers/DatabaseService/DatabaseService/settings.py
+++ Servers/DatabaseService/DatabaseService/settings.py
@@ -23,11 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zmatnp^g2sja-jtr$+bzc_f)$!#3%#75ra)noqu7tdfh5f8fx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-CORS_ORIGIN_ALLOW_ALL = True
 DEBUG = True
 APPEND_SLASH = True
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'https://gq.stokkebro.dev',
+    'http://localhost:8080'
+]
 
 
 # Application definition
